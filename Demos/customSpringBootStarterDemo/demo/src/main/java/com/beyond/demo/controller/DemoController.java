4588a939c2d4a76cd093a9491e391e6125279de6
beyondlov1 <beyondlov1@hotmail.com>
2019-07-15 21:11:14 +0800

    auto_commit

--- Demos/customSpringBootStarterDemo/demo/src/main/java/com/beyond/demo/controller/DemoController.java
+++ Demos/customSpringBootStarterDemo/demo/src/main/java/com/beyond/demo/controller/DemoController.java
@@ -1,18 +1,11 @@
 package com.beyond.demo.controller;
 
-import com.beyond.demo.mapper.tables.records.TestRecord;
 import org.jooq.DSLContext;
-import org.jooq.Result;
-import org.jooq.Schema;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
-
-import java.util.ArrayList;
-import java.util.List;
-
-import static com.beyond.demo.mapper.Tables.TEST;
 
 
 /**
@@ -25,20 +18,20 @@ public class DemoController {
     @Autowired
     DSLContext create;
 
-    @RequestMapping("/test")
-    public Object test() {
-        Schema schema = TEST.getSchema();
-        System.out.println(schema.getTables().get(1).getName());
-        this.create.insertInto(TEST).set(TEST.ID,"23434").set(TEST.USERNAME,"username2").set(TEST.PASSWORD,"password2").execute();
-        Result<TestRecord> result = this.create.selectFrom(TEST).fetch();
-        List<String> list = new ArrayList<>();
-        for (TestRecord testRecord : result) {
-            String username = testRecord.getUsername();
-            list.add(username);
-        }
-
-        return list;
-    }
+//    @RequestMapping("/test")
+//    public Object test() {
+//        Schema schema = TEST.getSchema();
+//        System.out.println(schema.getTables().get(1).getName());
+//        this.create.insertInto(TEST).set(TEST.ID,"23434").set(TEST.USERNAME,"username2").set(TEST.PASSWORD,"password2").execute();
+//        Result<TestRecord> result = this.create.selectFrom(TEST).fetch();
+//        List<String> list = new ArrayList<>();
+//        for (TestRecord testRecord : result) {
+//            String username = testRecord.getUsername();
+//            list.add(username);
+//        }
+//
+//        return list;
+//    }
 
 
     @Autowired
@@ -47,5 +40,11 @@ public class DemoController {
     @RequestMapping("/testRest")
     public Object testRest() {
         return restTemplate.getForEntity("http://www.baidu.com", String.class);
+    }
+
+    @RequestMapping("/testPathVariable/{id}")
+    public Object testPathVariable(@PathVariable String id){
+        System.out.println(id);
+        return id;
     }
 }
