92ef8256484a210debe912398a5b734d54dace4c
beyondlov1 <beyondlov1@hotmail.com>
2019-07-20 21:00:09 +0800

    auto_commit

--- Demos/customSpringBootStarterDemo/demo/src/test/java/com/beyond/demo/DemoApplicationTests.java
+++ Demos/customSpringBootStarterDemo/demo/src/test/java/com/beyond/demo/DemoApplicationTests.java
@@ -1,16 +1,19 @@
 package com.beyond.demo;
 
 import org.junit.Test;
-import org.junit.runner.RunWith;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.junit4.SpringRunner;
 
-@RunWith(SpringRunner.class)
-@SpringBootTest
+//@RunWith(SpringRunner.class)
+//@SpringBootTest
 public class DemoApplicationTests {
 
     @Test
     public void contextLoads() {
     }
 
+    @Test
+    public void test(){
+        String source = ". dfaeg\n";
+        boolean matches = source.matches("\\d+\\..*\\n?");
+        System.out.println(matches);
+    }
 }
