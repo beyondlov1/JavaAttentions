cd04b137a829df7bb91ba1d04590a6c68505c7db
beyondlov1 <beyondlov1@hotmail.com>
2019-08-12 21:00:05 +0800

    auto_commit

--- Demos/mybatisDemo/src/main/java/com/beyond/demo/PageInterceptor.java
+++ Demos/mybatisDemo/src/main/java/com/beyond/demo/PageInterceptor.java
@@ -1,13 +1,17 @@
 package com.beyond.demo;
 
+import org.apache.ibatis.builder.StaticSqlSource;
 import org.apache.ibatis.executor.Executor;
 import org.apache.ibatis.mapping.BoundSql;
 import org.apache.ibatis.mapping.MappedStatement;
+import org.apache.ibatis.mapping.SqlSource;
 import org.apache.ibatis.plugin.*;
 import org.apache.ibatis.session.ResultHandler;
 import org.apache.ibatis.session.RowBounds;
 
 import java.lang.reflect.Field;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Properties;
 
 @Intercepts(
@@ -21,16 +25,23 @@ public class PageInterceptor implements Interceptor {
     @Override
     public Object intercept(Invocation invocation) throws Throwable {
         Object[] args = invocation.getArgs();
+        if (args[2] == RowBounds.DEFAULT){
+            return invocation.proceed();
+        }
         MappedStatement mappedStatement = (MappedStatement) args[0];
         BoundSql boundSql = mappedStatement.getBoundSql(args[1]);
         String sql = boundSql.getSql();
         RowBounds rowBounds = (RowBounds) args[2];
-        String limit = String.format(" LIMIT %s %s", rowBounds.getOffset(), rowBounds.getLimit());
+        String limit = String.format(" LIMIT %s,%s", rowBounds.getOffset(), rowBounds.getLimit());
+        if (sql.endsWith(";")){
+            sql = sql.substring(0,sql.lastIndexOf(";"));
+        }
         sql = sql+limit;
-        Class<? extends BoundSql> boundSqlClass = boundSql.getClass();
-        Field sqlField = boundSqlClass.getDeclaredField("sql");
-        sqlField.setAccessible(true);
-        sqlField.set(boundSql,sql);
+        SqlSource sqlSource = new StaticSqlSource(mappedStatement.getConfiguration(),sql);
+        Field sqlSourceField = MappedStatement.class.getDeclaredField("sqlSource");
+        sqlSourceField.setAccessible(true);
+        sqlSourceField.set(mappedStatement,sqlSource);
+        args[2] = RowBounds.DEFAULT;
         return invocation.proceed();
     }
 
