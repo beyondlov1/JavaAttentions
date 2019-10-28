6a0983fb23daea68dea1e736ca12fa480b5000b1
beyondlov1 <beyondlov1@hotmail.com>
2019-10-28 21:00:06 +0800

    auto_commit

--- Demos/solr-spring-demo/src/main/java/com/beyond/solrspringdemo/solr/SolrController.java
+++ Demos/solr-spring-demo/src/main/java/com/beyond/solrspringdemo/solr/SolrController.java
@@ -1,5 +1,8 @@
 package com.beyond.solrspringdemo.solr;
 
+import org.apache.solr.client.solrj.SolrQuery;
+import org.apache.solr.client.solrj.SolrRequest;
+import org.apache.solr.client.solrj.SolrServerException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.solr.core.SolrTemplate;
 import org.springframework.data.solr.core.query.Criteria;
@@ -8,6 +11,8 @@ import org.springframework.data.solr.core.query.SimpleQuery;
 import org.springframework.data.solr.core.query.result.ScoredPage;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.io.IOException;
 
 
 /**
@@ -29,5 +34,16 @@ public class SolrController {
         ScoredPage<User> userPage = solrTemplate.queryForPage("new_core", query, User.class);
         userPage.get().forEach((System.out::println));
         return userPage;
+    }
+
+    @RequestMapping("/query2")
+    public Object query2() throws IOException, SolrServerException {
+        SolrQuery solrQuery = new SolrQuery("*:*");
+//        solrQuery.set("q","namevalue");
+//        solrQuery.set("qf", "name");
+        solrQuery.addFilterQuery("name:*catch");
+//        solrQuery.set("elevateIds", "3423");
+        solrTemplate.getSolrClient().query("new_core",solrQuery, SolrRequest.METHOD.POST);
+        return 1;
     }
 }
