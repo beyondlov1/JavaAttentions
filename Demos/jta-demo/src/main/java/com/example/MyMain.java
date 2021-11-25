880f6927c094bb66de1151a17f10bb1012438d3f
chenshipeng <chenshipeng@ysbang.cn>
2021-11-25 13:00:04 +0800

    auto

--- Demos/jta-demo/src/main/java/com/example/MyMain.java
+++ Demos/jta-demo/src/main/java/com/example/MyMain.java
@@ -1,8 +1,15 @@
 package com.example;
 
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
 /**
  * @author chenshipeng
  * @date 2021/11/25
  */
+@SpringBootApplication
 public class MyMain {
+    public static void main(String[] args) {
+        SpringApplication.run(MyMain.class, args);
+    }
 }
